25411398f5a0bcf14b79997ff5159c1292d72ae7
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-07-12 17:33:45 +0300

    feat: experiment with on fly hls transcoding

--- services/ingest/internal/api/consumer/whip/handler.go
+++ services/ingest/internal/api/consumer/whip/handler.go
@@ -1,17 +1,24 @@
 package whip
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os/exec"
 	"strings"
 	"sync"
 	"sync/atomic"
+	"syscall"
+	"time"
 
 	"github.com/pion/rtp"
+	"github.com/pion/rtp/codecs"
 	"github.com/pion/webrtc/v3"
+	"github.com/pion/webrtc/v3/pkg/media/h264writer"
+	"github.com/pion/webrtc/v3/pkg/media/samplebuilder"
 )
 
 // Each broadcaster sends sdp offer with `m=` field which describe stream as example:
@@ -129,38 +136,103 @@ func videoWriter(remoteTrack *webrtc.TrackRemote, peerConnection *webrtc.PeerCon
 		id = defaultVideoTrack
 	}
 
-	rtpBuf := make([]byte, 1500)
-	rtpPkt := &rtp.Packet{}
-	lastTimestamp := uint32(0)
+	// rtpBuf := make([]byte, 1500)
+	// rtpPkt := &rtp.Packet{}
+	// lastTimestamp := uint32(0)
+
+	// videoTrack, _ := webrtc.NewTrackLocalStaticSample(webrtc.RTPCodecCapability{MimeType: webrtc.MimeTypeH264}, "video", "pion")
+
+	ffmpeg := exec.Command("ffmpeg",
+		"-f", "lavfi", "-re", 
+		"-i", "anullsrc=channel_layout=stereo:sample_rate=44100",
+		"-i", "pipe:0",
+		"-c:a", "aac",
+		"-loglevel", "debug",
+		"-c:v", "libx264",
+		"-preset", "ultrafast",
+		"-crf", "30",
+		"-maxrate", "3000k",
+		"-bufsize", "6000k",
+		"-pix_fmt", "yuv420p",
+		"-an",
+		"-f", "hls",
+		"-hls_time", "8", 
+		"-hls_list_size", "4",
+		"-hls_flags", "delete_segments",
+		"-hls_start_number_source", "datetime",
+		"-hls_segment_filename", "output_%03d.ts",
+		"output.m3u8")
+
+	stdin, _ := ffmpeg.StdinPipe()
+	// NOTE: to make multiple stdin just make it again. In ffmpeg should pipe:0, pipe:1 first pipe will be 0 index
+
+	stderr, _ := ffmpeg.StderrPipe()
+
+	writer := h264writer.NewWith(stdin)
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	go func () {
+		if err := ffmpeg.Run(); err != nil {
+			log.Println("Error when run ", err)
+		}
+	}()
+
+
 	for {
-		rtpRead, _, err := remoteTrack.Read(rtpBuf)
+		// rtpRead, _, err := remoteTrack.Read(rtpBuf)
 
 		// log.Println("Write video")
 
-		switch {
-		case errors.Is(err, io.EOF):
-			return
-		case err != nil:
-			log.Println(err)
-			return
-		}
+		// switch {
+		// case errors.Is(err, io.EOF):
+		// 	return
+		// case err != nil:
+		// 	log.Println(err)
+		// 	return
+		// }
 
-		if err = rtpPkt.Unmarshal(rtpBuf[:rtpRead]); err != nil {
-			log.Println(err)
-			return
-		}
+		// if err = rtpPkt.Unmarshal(rtpBuf[:rtpRead]); err != nil {
+		// 	log.Println(err)
+		// 	return
+		// }
 
-		timeDiff := rtpPkt.Timestamp - lastTimestamp
-		if lastTimestamp == 0 {
-			timeDiff = 0
-		}
-		lastTimestamp = rtpPkt.Timestamp
+		rtp, _, _ := remoteTrack.ReadRTP()
+
+		// videoBuilder.Push(rtp)
+
+		// sample := videoBuilder.Pop()
+
+		// if sample == nil {
+		// 	continue
+		// }
+
+		// timeDiff := rtpPkt.Timestamp - lastTimestamp
+		// if lastTimestamp == 0 {
+		// 	timeDiff = 0
+		// }
+		// lastTimestamp = rtpPkt.Timestamp
 
 		s.VideoChannel.channelMutex.RLock()
-		s.VideoChannel.SendVideoPacket(rtpPkt, id, timeDiff)
+		// NOTE: One way to write it into ffmpeg
+		writer.WriteRTP(rtp)
+
+		// videoTrack.WriteSample(*sample)
 		s.VideoChannel.channelMutex.RUnlock()
 	}
 }
+
+var (
+	audioBuilder, videoBuilder     *samplebuilder.SampleBuilder
+	audioTimestamp, videoTimestamp time.Duration
+	streamKey                      string
+)
 
 func (h *handler) Handler(res http.ResponseWriter, r *http.Request) {
 	h.streamMutex.Lock()
@@ -182,6 +254,14 @@ func (h *handler) Handler(res http.ResponseWriter, r *http.Request) {
 
 	peerConnection, err := h.webrtcAPI.NewPeerConnection(webrtc.Configuration{})
 
+	videoBuilder = samplebuilder.New(10, &codecs.H264Packet{}, 90000)
+
+	pipePath := "test"
+
+	if err := syscall.Mkfifo(pipePath, 0666); err != nil {
+		fmt.Println("Error creating data pipe:", err)
+	}
+
 	stream := GetStream(streamKey)
 
 	peerConnection.OnTrack(
@@ -196,7 +276,6 @@ func (h *handler) Handler(res http.ResponseWriter, r *http.Request) {
 
 			if strings.HasPrefix(track.Codec().RTPCodecCapability.MimeType, "audio") {
 				audioWriter(track, stream)
-
 			} else {
 				videoWriter(track, peerConnection, stream)
 			}
