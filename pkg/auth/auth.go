1a42781b639148b39fdc4666c8b5b3313ac0f5fa
romashorodok <108732029+romashorodok@users.noreply.github.com>
2023-08-08 23:12:02 +0300

    feat: Added stub auth provider for openapi spec request passing

---
+++ pkg/auth/auth.go
@@ -0,0 +1,198 @@
+package auth
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/deepmap/oapi-codegen/pkg/ecdsafile"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/jws"
+	"github.com/lestrrat-go/jwx/jwt"
+)
+
+type JWSValidator interface {
+	ValidateJWS(jws string) (jwt.Token, error)
+}
+
+const JWTClaimsContextKey = "jwt_claims"
+
+var (
+	ErrNoAuthHeader      = errors.New("Authorization header is missing")
+	ErrInvalidAuthHeader = errors.New("Authorization header is malformed")
+	ErrClaimsInvalid     = errors.New("Provided claims do not match expected scopes")
+)
+
+func GetJWSFromRequest(req *http.Request) (string, error) {
+	authHdr := req.Header.Get("Authorization")
+	if authHdr == "" {
+		return "", ErrNoAuthHeader
+	}
+	prefix := "Bearer "
+	if !strings.HasPrefix(authHdr, prefix) {
+		return "", ErrInvalidAuthHeader
+	}
+	return strings.TrimPrefix(authHdr, prefix), nil
+}
+
+func NewAuthenticator(v JWSValidator) openapi3filter.AuthenticationFunc {
+	return func(ctx context.Context, input *openapi3filter.AuthenticationInput) error {
+		return Authenticate(v, ctx, input)
+	}
+}
+
+func Authenticate(v JWSValidator, ctx context.Context, input *openapi3filter.AuthenticationInput) error {
+
+	if input.SecuritySchemeName != "BearerAuth" {
+		return fmt.Errorf("security scheme %s != 'BearerAuth'", input.SecuritySchemeName)
+	}
+
+	jws, err := GetJWSFromRequest(input.RequestValidationInput.Request)
+	if err != nil {
+		return fmt.Errorf("getting jws: %w", err)
+	}
+
+	token, err := v.ValidateJWS(jws)
+	if err != nil {
+		return fmt.Errorf("validating JWS: %w", err)
+	}
+
+	err = CheckTokenClaims(input.Scopes, token)
+
+	if err != nil {
+		return fmt.Errorf("token claims don't match: %w", err)
+	}
+
+	return nil
+}
+
+func GetClaimsFromToken(t jwt.Token) ([]string, error) {
+	rawPerms, found := t.Get(PermissionsClaim)
+
+	if !found {
+		return make([]string, 0), nil
+	}
+
+	rawList, ok := rawPerms.([]interface{})
+	if !ok {
+		return nil, fmt.Errorf("'%s' claim is unexpected type'", PermissionsClaim)
+	}
+
+	claims := make([]string, len(rawList))
+
+	for i, rawClaim := range rawList {
+		var ok bool
+		claims[i], ok = rawClaim.(string)
+		if !ok {
+			return nil, fmt.Errorf("%s[%d] is not a string", PermissionsClaim, i)
+		}
+	}
+	return claims, nil
+}
+
+func CheckTokenClaims(expectedClaims []string, t jwt.Token) error {
+	claims, err := GetClaimsFromToken(t)
+	if err != nil {
+		return fmt.Errorf("getting claims from token: %w", err)
+	}
+
+	claimsMap := make(map[string]bool, len(claims))
+	for _, c := range claims {
+		claimsMap[c] = true
+	}
+
+	for _, e := range expectedClaims {
+		if !claimsMap[e] {
+			return ErrClaimsInvalid
+		}
+	}
+	return nil
+}
+
+const PrivateKey = `-----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIN2dALnjdcZaIZg4QuA6Dw+kxiSW502kJfmBN3priIhPoAoGCCqGSM49
+AwEHoUQDQgAE4pPyvrB9ghqkT1Llk0A42lixkugFd/TBdOp6wf69O9Nndnp4+HcR
+s9SlG/8hjB2Hz42v4p3haKWv3uS1C6ahCQ==
+-----END EC PRIVATE KEY-----`
+
+const KeyID = `fake-key-id`
+const FakeIssuer = "fake-issuer"
+const FakeAudience = "example-users"
+const PermissionsClaim = "perm"
+
+type FakeAuthenticator struct {
+	PrivateKey *ecdsa.PrivateKey
+	KeySet     jwk.Set
+}
+
+var _ JWSValidator = (*FakeAuthenticator)(nil)
+
+func NewFakeAuthenticator() (*FakeAuthenticator, error) {
+	privKey, err := ecdsafile.LoadEcdsaPrivateKey([]byte(PrivateKey))
+	if err != nil {
+		return nil, fmt.Errorf("loading PEM private key: %w", err)
+	}
+
+	set := jwk.NewSet()
+	pubKey := jwk.NewECDSAPublicKey()
+
+	err = pubKey.FromRaw(&privKey.PublicKey)
+	if err != nil {
+		return nil, fmt.Errorf("parsing jwk key: %w", err)
+	}
+
+	err = pubKey.Set(jwk.AlgorithmKey, jwa.ES256)
+	if err != nil {
+		return nil, fmt.Errorf("setting key algorithm: %w", err)
+	}
+
+	err = pubKey.Set(jwk.KeyIDKey, KeyID)
+	if err != nil {
+		return nil, fmt.Errorf("setting key ID: %w", err)
+	}
+
+	set.Add(pubKey)
+
+	return &FakeAuthenticator{PrivateKey: privKey, KeySet: set}, nil
+}
+
+func (f *FakeAuthenticator) ValidateJWS(jwsString string) (jwt.Token, error) {
+	return jwt.Parse([]byte(jwsString), jwt.WithKeySet(f.KeySet),
+		jwt.WithAudience(FakeAudience), jwt.WithIssuer(FakeIssuer))
+}
+
+func (f *FakeAuthenticator) SignToken(t jwt.Token) ([]byte, error) {
+	hdr := jws.NewHeaders()
+	if err := hdr.Set(jws.AlgorithmKey, jwa.ES256); err != nil {
+		return nil, fmt.Errorf("setting algorithm: %w", err)
+	}
+	if err := hdr.Set(jws.TypeKey, "JWT"); err != nil {
+		return nil, fmt.Errorf("setting type: %w", err)
+	}
+	if err := hdr.Set(jws.KeyIDKey, KeyID); err != nil {
+		return nil, fmt.Errorf("setting Key ID: %w", err)
+	}
+	return jwt.Sign(t, jwa.ES256, f.PrivateKey, jwt.WithHeaders(hdr))
+}
+
+func (f *FakeAuthenticator) CreateJWSWithClaims(claims []string) ([]byte, error) {
+	t := jwt.New()
+	err := t.Set(jwt.IssuerKey, FakeIssuer)
+	if err != nil {
+		return nil, fmt.Errorf("setting issuer: %w", err)
+	}
+	err = t.Set(jwt.AudienceKey, FakeAudience)
+	if err != nil {
+		return nil, fmt.Errorf("setting audience: %w", err)
+	}
+	err = t.Set(PermissionsClaim, claims)
+	if err != nil {
+		return nil, fmt.Errorf("setting permissions: %w", err)
+	}
+	return f.SignToken(t)
+}
